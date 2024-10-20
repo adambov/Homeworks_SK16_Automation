4af57c5e6254fca4a909aa02c9bcf8a56f79fb1b
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2024-10-20 14:23:50 +0300

    adding shorter version of task 5

--- src/homework_6/Task5_ToDoList.java
+++ src/homework_6/Task5_ToDoList.java
@@ -1,6 +1,5 @@
 package homework_6;
 
-import java.security.KeyStore;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,78 +12,39 @@ public class Task5_ToDoList {
 
         ArrayList<String> Todo = new ArrayList<>();
         boolean isFinished = false;
-        String state = "is finished";
 
         for (String task : tasks) {
             Todo.add(task);
         }
-//        System.out.println(Todo);
 
         Scanner scanner2 = new Scanner(System.in);
         System.out.println("add, remove or finish a task: ");
-        String action = scanner1.nextLine();
+        String action = scanner2.nextLine();
 
-        if (action.equals("add")) {
-            Scanner scanner3 = new Scanner(System.in);
-            System.out.println("Enter a new task: ");
-            String NewTask = scanner3.nextLine();
-            Todo.add(NewTask);
-            scanner3.close();
-        } else if (action.equals("remove")) {
-            Scanner scanner4 = new Scanner(System.in);
-            System.out.println("Remove a task: ");
-            String RemovedTask = scanner4.nextLine();
-            Todo.remove(RemovedTask);
-            scanner4.close();
-        } else if (action.equals("finish")) {
-            Scanner scanner5 = new Scanner(System.in);
-            System.out.println("Finish a task: ");
-            String CompletedTask = scanner5.nextLine();
-            isFinished = true;
-            Todo.set(Todo.indexOf(CompletedTask), "*" + CompletedTask + "*");
-            System.out.println(CompletedTask + " is finished.");
-            scanner5.close();
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Enter a new task: ");
 
-        } else {
-            System.out.println("invalid input!");
+            if (action.equals("add")) {
+
+                String NewTask = scanner3.nextLine();
+                Todo.add(NewTask);
+
+            } else if (action.equals("remove")) {
+
+                String RemovedTask = scanner3.nextLine();
+                Todo.remove(RemovedTask);
+
+            } else if (action.equals("finish")) {
+
+                String CompletedTask = scanner3.nextLine();
+                isFinished = true;
+                Todo.set(Todo.indexOf(CompletedTask), "*" + CompletedTask + "*");
+                System.out.println(CompletedTask + " is finished.");
+
+
+            } else {
+                System.out.println("invalid input!");
+            }
+            System.out.println(Todo);
         }
-        System.out.println(Todo);
     }
-}
-// трябва да направя ТоДу лист
-// трябва да може юзъра да добавя таскове
-// да премахва таскове
-// да ги маркира като завършени
-// да принтира новото състояние на листа
-
-// за целта ми трябва скенър, и иф условие, което да търси думите:
-//add, remove, completed
-
-//        while (action.equals("add")) {
-//            Scanner scanner2 = new Scanner(System.in);
-//            System.out.println("Enter a task: ");
-//            String NewAction = scanner2.nextLine();
-//            Todo.add(NewAction);
-//            if (NewAction.equals("stop")) {
-//                System.out.println(Todo);
-//                    continue;
-//            }
-//            if (NewAction.equals("remove")) {
-//                Scanner scanner3 = new Scanner(System.in);
-//                System.out.println("Remove a task: ");
-//                String RemovedTask = scanner3.nextLine();
-//                Todo.remove(RemovedTask);
-//                scanner3.close();
-//
-//            }
-//            if (NewAction.equals("complete")) {
-//                Scanner scanner4 = new Scanner(System.in);
-//                System.out.println("Complete a task: ");
-//                String CompletedTask = scanner4.nextLine();
-//                boolean isCompleted = true;
-//                String completed = "is Done";
-//                System.out.println(CompletedTask + completed);
-//                scanner4.close();
-//            }
-//            System.out.println(Todo);
-//        }
